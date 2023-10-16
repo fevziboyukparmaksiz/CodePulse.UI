04c27a9044c7088a2ab9663c031ffb0d994c1d5c
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-10-16 22:49:56 +0300

    initial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { CategoryListComponent } from './features/category/category-list/category-list.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AddCategoryComponent } from './features/category/add-category/add-category.component';
+
+const routes: Routes = [
+  {
+    path: 'admin/categories',
+    component: CategoryListComponent
+  },
+
+  {
+    path: 'admin/categories/add',
+    component: AddCategoryComponent
+  }
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
