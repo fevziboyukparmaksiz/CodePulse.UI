e93ccaac626defec3d8a13747370ecc69bb3cca5
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-10-17 00:14:28 +0300

    Get All Categories

--- src/app/features/category/add-category/add-category.component.ts
+++ src/app/features/category/add-category/add-category.component.ts
@@ -2,6 +2,7 @@ import { CategoryService } from './../services/category.service';
 import { Component, OnDestroy } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-category',
@@ -14,7 +15,8 @@ export class AddCategoryComponent implements OnDestroy {
   private addCategorySubscription?: Subscription
 
 
-  constructor(private categoryService: CategoryService) {
+  constructor(private categoryService: CategoryService,
+    private Router: Router) {
     this.model = {
       name: '',
       urlHandle: ''
@@ -25,7 +27,7 @@ export class AddCategoryComponent implements OnDestroy {
     this.addCategorySubscription = this.categoryService.addCategory(this.model)
       .subscribe({
         next: (response) => {
-          console.log("This was successful!");
+          this.Router.navigateByUrl('/admin/categories');
         }
       });
   }
