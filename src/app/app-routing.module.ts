d54479295117207e2feeff3c2b62ddf81827f209
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-10-18 21:44:22 +0300

    Create Blog Post List Component and Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddCategoryComponent } from './features/category/add-category/add-category.component';
 import { EditCategoryComponent } from './features/category/edit-category/edit-category.component';
+import { BlogpostListComponent } from './features/blog-post/blogpost-list/blogpost-list.component';
 
 const routes: Routes = [
   {
@@ -16,8 +17,11 @@ const routes: Routes = [
   {
     path: 'admin/categories/:id',
     component: EditCategoryComponent
+  },
+  {
+    path: 'admin/blogposts',
+    component: BlogpostListComponent
   }
-
 
 ];
 
