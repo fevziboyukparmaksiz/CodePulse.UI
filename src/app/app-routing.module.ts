86bef83975f09e9b2e1e75efa87e8b0fdd8ac2a1
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-10-18 15:13:49 +0300

    Created Edit-Category and pass route parameters

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,20 @@ import { CategoryListComponent } from './features/category/category-list/categor
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddCategoryComponent } from './features/category/add-category/add-category.component';
+import { EditCategoryComponent } from './features/category/edit-category/edit-category.component';
 
 const routes: Routes = [
   {
     path: 'admin/categories',
     component: CategoryListComponent
   },
-
   {
     path: 'admin/categories/add',
     component: AddCategoryComponent
+  },
+  {
+    path: 'admin/categories/:id',
+    component: EditCategoryComponent
   }
 
 
