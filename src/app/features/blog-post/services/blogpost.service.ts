fe29315b4059e2112ea58564ad5c23ced5ce6d2f
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-10-18 23:17:30 +0300

    Create BlogPost Service

---
+++ src/app/features/blog-post/services/blogpost.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AddBlogPost } from '../models/add-blog-post.model';
+import { environment } from 'src/environments/environment.development';
+import { BlogPost } from '../models/blog-post.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogpostService {
+
+  constructor(private http: HttpClient) { }
+
+  createBlogPost(model: AddBlogPost): Observable<BlogPost> {
+    return this.http.post<BlogPost>(`${environment}/api/blogposts`, model)
+  }
+}
